8c56a0b4eaab14327c9ed5de558864864553bef7
S_K_Khan <159645700+BelimSahilkhan@users.noreply.github.com>
2024-11-26 05:12:31 -1100

    Add files via upload

---
+++ HelloW.cs
@@ -0,0 +1,22 @@
+using System;
+class HelloW
+{
+	static void Main(string[] args)
+	{
+		int a=10;
+		int b=20;
+		int Sum=a+b;
+		int Sub=a-b;
+		int Mul=a*b;
+		int Div=a/b;
+		Console.WriteLine("Sum Is=>"+Sum);
+		Console.WriteLine("Sub Is=>"+Sub);
+		Console.WriteLine("Mul Is=>"+Mul);
+		Console.WriteLine("Div Is=>"+Div);
+
+		/*Console.WriteLine("Enter Your Name");
+		string inp=Console.ReadLine();	
+		Console.WriteLine(inp);*/
+		Console.ReadLine();
+	}
+}
